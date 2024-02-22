ffc40f3d300c01810b8038e73224231bbdf6f6d4
GostryMaxim <rotoff7@mail.ru>
2024-02-23 01:09:31 +0300

    JavaAdvanced - Task#5 Complete

--- src/main/java/additionalTasks/task5/Main.java
+++ src/main/java/additionalTasks/task5/Main.java
@@ -6,31 +6,27 @@ import java.util.*;
 
 public class Main {
 
-    // Вычитывание слов из файла + сортировка + подсчет кол-во повторений слов в файле и вывод этой статистики
-    //TODO Вывод сортированного списка по 5-10 слов в одной строке
-    //TODO Найти самое частое слово и вывести его на консоль вместе с кол-вом повторений
-    //TODO Раскидать методы по классам, проверить нейминг
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("src/main/resources/Words.txt");
-        Manipulations fileActions = new Manipulations();
-        ArrayList<String> textFromFile;
-
-        textFromFile = parsFile(file);
+        Manipulations actions = new Manipulations();
+        ArrayList<String> textFromFile = parseFile(file);
 
         Collections.sort(textFromFile);
-        System.out.println("Отсортированный тектс файла:");
-        fileActions.printWordList(textFromFile);
 
-        Set<String> set = new HashSet<>(textFromFile);
+        System.out.println("\nОтсортированный текст файла:");
+        actions.printWordList(textFromFile);
 
-        Map<String, Integer> wordsCountMap = fileActions.wordsCount(set, textFromFile);
+        Map<String, Integer> wordsCountMap = actions.wordsCount(textFromFile);
 
         for (Map.Entry<String, Integer> entry : wordsCountMap.entrySet()) {
             System.out.println("Слово \"" + entry.getKey() + "\" встречается в файле: " + entry.getValue() + " раз(а).");
         }
+        System.out.println("-------------------------------------------------");
+
+        actions.printMostPopularWord(wordsCountMap);
     }
 
-    public static ArrayList<String> parsFile(File file) throws FileNotFoundException {
+    private static ArrayList<String> parseFile(File file) throws FileNotFoundException {
         Scanner scanner = new Scanner(file);
         ArrayList<String> wordList = new ArrayList<>();
         while (scanner.hasNext()) {
--- src/main/java/additionalTasks/task5/Manipulations.java
+++ src/main/java/additionalTasks/task5/Manipulations.java
@@ -7,26 +7,67 @@ public class Manipulations {
     public Manipulations() {
     }
 
-    public Map<String, Integer> wordsCount(Set<String> list, ArrayList<String> words){
+    /**
+     * Сперва создаем set и отсекает дубликаты слов,
+     * записываем слова в ключ map-ы и выставляем всем кол-во повторений = 0.
+     * После чего проходим по нашему массива слов (с повторениями)
+     * и для каждого совпадения увеличиваем каунтер в мапе на 1
+     *
+     * @param words наш массив слов из файла
+     * @return map возвращает пары: слово - кол-во повтороений
+     */
+    public Map<String, Integer> wordsCount(ArrayList<String> words) {
         Map<String, Integer> map = new HashMap<>();
-        for (String value:list){
+        Set<String> set = new HashSet<>(words);
+        for (String value : set) {
             map.put(value, 0);
         }
-        for (String word:words) {
-            if (map.containsKey(word)){
-                map.put(word, map.get(word)+1);
+        // Не очень нравится двойной проход по массивам.
+        // Думаю, что тут можно выполнить подсчет за один цикл, но идея реализации не пришла
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
             }
         }
         return map;
     }
 
-    public void printWordList(ArrayList<String> list){
-        for (String word: list) {
-            System.out.println(word);
+    public void printMostPopularWord(Map<String, Integer> map) {
+        int maxCount = Collections.max(map.values());
+        for (Map.Entry<String, Integer> pair : map.entrySet()) {
+            if (pair.getValue() == maxCount) {
+                System.out.printf("Самое часто встречаемое слово = %s. Встречается в файле - %d раз(а).",
+                        pair.getKey(), pair.getValue());
+                break;
+            }
         }
     }
 
+    public void printWordList(ArrayList<String> list) {
+        StringBuilder oneLine = lineBuilder(list);
+        System.out.println(oneLine);
+        System.out.println();
+    }
 
+    /**
+     * Метод создает экземпляр StringBuilder-а и записывает в него по 10 слов на одну строку.
+     * @param list Принимает распарсенный по словам файл
+     * @return выводим нужный для вывода в консоль StringBuilder
+     */
+    private StringBuilder lineBuilder(ArrayList<String> list) {
+        StringBuilder oneLine = new StringBuilder();
+        int temp = 0;
+        for (String word : list) {
+            if (temp != 9) {
+                oneLine.append(word).append(", ");
+                temp++;
+            } else {
+                oneLine.append(word).append("\n");
+                temp = 0;
+            }
+        }
+        return oneLine;
+    }
 
 
 }
